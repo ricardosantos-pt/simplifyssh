66d67f1fffede90ef7924933bf4bf2a4c2ad443b
Ricardo <ricardo.santos9521@outlook.com>
2019-09-20 18:38:14 +0100

    add install_requires

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup(
-    name = 'simplifyssh',
-    version = '0.1.0',
-    packages = ['simplifyssh'],
-    entry_points = {
+    name='simplifyssh',
+    version='0.1.0',
+    packages=['simplifyssh'],
+    install_requires=[
+        'paramiko'
+    ],
+    entry_points={
         'console_scripts': [
             'simplifyssh = simplifyssh.__main__:main'
         ]
