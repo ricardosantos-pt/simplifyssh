bd5ba44b6beb221aab1c491113e582b7bebea7cc
Ricardo Santos <ricardo.santos9521@outlook.com>
2020-05-08 22:28:05 +0100

    simplifyssh command not working

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from setuptools import setup
 
 setup(
     name='simplifyssh',
-    version='0.2.3',
+    version='0.3.0',
     packages=['simplifyssh'],
     install_requires=[
         'paramiko'
-    ]
+    ],
+    entry_points={
+        'console_scripts': [
+            'simplifyssh = simplifyssh.__main__:main'
+        ]
+    }
 )
